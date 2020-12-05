1c8f0ad06c0fe8b9695a209da02f6b1116df4ed3
Antonio Muñoz <antoniogmc@gmail.com>
2020-12-05 20:38:52 +0100

    day2 part2 solution

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -2,9 +2,11 @@ package adventofcode
 
 import scala.io.Source
 import scala.collection.mutable.ArraySeq
+import scala.annotation.tailrec
 
 object Day2 {
 
+  @tailrec
   def computer(program: ArraySeq[Int], current: Int = 0): ArraySeq[Int] = {
     program(current) match {
       case 1 => {
@@ -25,6 +27,13 @@ object Day2 {
 
   var program = Source.fromFile("src/main/resources/input-day2.txt").getLines
     .flatMap(_.split(',')).map(_.toInt).toArray
+
+  def fix(noun: Int, verb: Int): ArraySeq[Int] = {
+    val fixed = program.to[ArraySeq]
+    fixed.update(1, noun)
+    fixed.update(2, verb)
+    fixed
+  }
 }
 
 object Day2Par1 extends App {
@@ -32,10 +41,22 @@ object Day2Par1 extends App {
 
   println("Day2 Part1")
 
-  val fixed = program.to[ArraySeq]
-  fixed.update(1, 12)
-  fixed.update(2, 2)
-  println(computer(fixed)(0))
+  println(computer(fix(12, 2))(0))
+}
+
+object Day2Par2 extends App {
+  import Day2._
+
+  println("Day2 Part2")
+
+  val inputs = for {
+    noun <- 0 to 99
+    verb <- 0 to 99
+  } yield fix(noun, verb)
+
+  val (noun, verb, _) = inputs.map(i => (i(1), i(2), computer(i)(0))).find(p => p._3 == 19690720).orNull
+
+  println(100 * noun + verb)
 }
 
 object Day2Test extends App {
