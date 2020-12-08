30c2bfab3f0b1e716c594e222c05fa94530c7955
Antonio Muñoz <antonio.munoz@clarity.ai>
2020-12-08 14:32:07 +0100

    Day7 part2 WIP

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -9,10 +9,8 @@ object Day7 {
   def compute(program: Array[Int]): List[Int] =
     (0 to 4).permutations.map(sequence(program, _)).toList
 
-  def computeFeedback(program: Array[Int]): List[Int] = {
-    val input = compute(program).max
-    (5 to 9).permutations.map(feedback(program, _, input)).toList
-  }
+  def computeFeedback(program: Array[Int]): List[List[Int]] = 
+    (5 to 9).permutations.map(feedback(program, _)).toList
 
   def sequence(program: Array[Int], sequence: IndexedSeq[Int]): Int = {
     val outA = runProgram(program, Input(sequence(0), 0))(0)
@@ -22,14 +20,12 @@ object Day7 {
     runProgram(program, Input(sequence(4), outD))(0)
   }
 
-  def feedback(program: Array[Int], sequence: IndexedSeq[Int], input: Int): Int = {
-    println(sequence)
-    val p = program.to[ArraySeq]
-    val outA = runProgram2(p, Input(sequence(0), input))(0)
-    val outB = runProgram2(p, Input(sequence(1), outA))(0)
-    val outC = runProgram2(p, Input(sequence(2), outB))(0)
-    val outD = runProgram2(p, Input(sequence(3), outC))(0)
-    runProgram2(p, Input(sequence(4), outD))(0)
+  def feedback(program: Array[Int], sequence: IndexedSeq[Int]): List[Int] = {
+    val outA = runProgram(program, Input(sequence(0), 0))
+    val outB = runProgram(program, Input(sequence(1), outA(0), outA(1), outA(2), outA(3), outA(4)))
+    val outC = runProgram(program, Input(sequence(2), outB(0), outB(1), outB(2), outB(3), outB(4)))
+    val outD = runProgram(program, Input(sequence(3), outC(0), outC(1), outC(2), outC(3), outC(4)))
+    runProgram(program, Input(sequence(4), outD(0), outD(1), outD(2), outD(3), outD(4))).toList
   }
 
   val program = loadProgram("input-day7.txt")
@@ -52,6 +48,7 @@ object Day7Part2 extends App {
 }
 
 object Day7Test extends App {
+  import Day5.{runProgram, Input}
   import Day7._
 
   val program1 = Array(3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0)
@@ -64,7 +61,8 @@ object Day7Test extends App {
 //  assert(compute(program3).max == 65210)
 
   val program4 = Array(3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5)
-  println(feedback(program4, IndexedSeq(9,8,7,6,5), 0))
+  println(runProgram(program4, Input(9, 0)))
+//  println(computeFeedback(program4))
 
   println("OK")
 }
