b1326e4cae205bb826f6f5edc0552298b1271e90
Antonio Muñoz <antonio.munoz@clarity.ai>
2020-12-15 17:25:42 +0100

    Day11 part2 solution

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -41,14 +41,14 @@ object Day11 {
 
   def move(position: Position, direction: Direction): Position = 
     (position, direction) match {
-      case ((x, y), Up) => (x, y + 1)
-      case ((x, y), Down) => (x, y - 1)
+      case ((x, y), Up) => (x, y - 1)
+      case ((x, y), Down) => (x, y + 1)
       case ((x, y), Left) => (x - 1, y)
       case ((x, y), Right) => (x + 1, y)
     }
 
   @tailrec
-  def paint(hull: Map[Position, Color], position: Position, direction: Direction, state: State): Map[Position, Color] = {
+  def run(hull: Map[Position, Color], position: Position, direction: Direction, state: State): Map[Position, Color] = {
 
     val current = hull.getOrElse(position, Black)
 
@@ -57,19 +57,36 @@ object Day11 {
     if (result.paused) {
       result.output match {
         case ListBuffer(0, 0) => {
-          paint(hull + (position -> Black), move(position, direction.left), direction.left, result)
+          run(hull + (position -> Black), move(position, direction.left), direction.left, result)
         }
         case ListBuffer(0, 1) => {
-          paint(hull + (position -> Black), move(position, direction.right), direction.right, result)
+          run(hull + (position -> Black), move(position, direction.right), direction.right, result)
         }
         case ListBuffer(1, 0) => {
-          paint(hull + (position -> White), move(position, direction.left), direction.left, result)
+          run(hull + (position -> White), move(position, direction.left), direction.left, result)
         }
         case ListBuffer(1, 1) => {
-          paint(hull + (position -> White), move(position, direction.right), direction.right, result)
+          run(hull + (position -> White), move(position, direction.right), direction.right, result)
         }
       }
     } else hull
+  }
+
+  def paint(hull: Map[Position, Color]): String = {
+    val minX = hull.keySet.minBy(_._1)._1
+    val maxX = hull.keySet.maxBy(_._1)._1
+    val minY = hull.keySet.minBy(_._2)._2
+    val maxY = hull.keySet.maxBy(_._2)._2
+
+    val grid = for {
+      y <- minY to maxY
+      x <- minX to maxX
+    } yield (x, y)
+
+    grid.map(hull.getOrElse(_, Black)).map {
+      case Black => ' '
+      case White => '#'
+    }.grouped(maxX + 1).map(_.mkString).mkString("\n")
   }
 }
 
@@ -81,7 +98,20 @@ object Day11Part1 extends App {
 
   val init = runProgram(program)
 
-  val hull = paint(Map.empty[Position, Color], (0, 0), Up, init)
+  val hull = run(Map.empty[Position, Color], (0, 0), Up, init)
 
   println(hull.size)
+}
+
+object Day11Part2 extends App {
+  import Day5.{runProgram, loadProgram, Program, Input, State}
+  import Day11._
+
+  val program = loadProgram("input-day11.txt")
+
+  val init = runProgram(program)
+
+  val hull = run(Map((0, 0) -> White), (0, 0), Up, init)
+
+  println(paint(hull))
 }
